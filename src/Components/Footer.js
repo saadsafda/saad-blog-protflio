a7d457aa2b1d188be2e11e9169fd91719f96231d
saadsafda <muhammadsaadsafdar2005@gmail.com>
2021-05-19 10:19:58 +0500

    protflio

---
+++ src/Components/Footer.js
@@ -0,0 +1,41 @@
+import React from "react";
+
+const Footer = ({ data }) => {
+  if (data) {
+    var networks = data.social.map(function (network) {
+      return (
+        <li key={network.name}>
+          <a href={network.url}>
+            <i className={network.className}></i>
+          </a>
+        </li>
+      );
+    });
+  }
+
+  return (
+    <footer>
+      <div className="row">
+        <div className="twelve columns">
+          <ul className="social-links">{networks}</ul>
+
+          <ul className="copyright">
+            <li>
+              Made by{" "}
+              <a title="Saad" target="_saad" href="https://saadsafda.github.io/saadcv/?fbclid=IwAR1QCXk6Rumhe7RwRh_9o7-mSVkN9TwIYvsrlj7hSRc1Q8HCPs6dwtC1gco">
+                Saad
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div id="go-top">
+          <a className="smoothscroll" title="Back to Top" href="#home">
+            <i className="icon-up-open"></i>
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
